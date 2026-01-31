19f7efab353e6649abc5dc6599dfd2e4ece79d42
Tello-P <perezgarrote.tello@gmail.com>
2026-01-31 19:38:11 +0100

    error message correction-is_operator added

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 #define INT 0
 #define OP 1
+#define ERR -1
 #define INT_STR "INT"
 #define OP_STR "OP"
 
@@ -22,6 +23,10 @@ bool is_digit(char c){
 
   return (c >= '0' && c <= '9');
 }
+bool is_operator(char c)
+{
+  return c=='+' || c == '-';
+}
 
 // obtiene el token actual y avanza el puntero de current_pos
 void get_next_token(char *text)
@@ -35,26 +40,30 @@ void get_next_token(char *text)
     // el y los valores de los numeros estan seguidos
     // por eso devuelve el numero correcto esta resta
   }
-  else {
+  else if (is_operator(c)){
     current_token.type = OP;
     current_token.number_value = 0;
     current_token.operator_value = c;
   }
+  else{
+    current_token.type = ERR;
+  }
+  current_pos += 1;
 }
 
 // Representa el token como un string s
 void token_to_str(Token token, char *s)
 {
-  sprintf(s, "Token (type=%s, number_value=%d, operator_value=%s)", INT ? INT_STR: OP_STR, token.number_value, &token.operator_value);
+  sprintf(s, "Token (type=%s, number_value=%d, operator_value=%c)",token.type == INT ? INT_STR: OP_STR, token.number_value, token.operator_value);
 }
 
 
 void parse(int type){
   if (current_token.type != type)
   { //PASAR A STRING EL VALOR DEL TOKEN PARA ENTENDER EL ERROR
-    char token_str[100];
-    token_to_str(current_token, token_str);
-    printf("Syntax error at pos %d: Expected token of type %d, but recieved %s\n",current_pos, type, token_str);
+    char s[100];
+    token_to_str(current_token, s);
+    printf("Syntax error at pos %d: Expected token of type %s, but recieved %s\n",current_pos, type == INT ? INT_STR : OP_STR, s);
     exit(EXIT_FAILURE);
   }
 }
@@ -64,8 +73,18 @@ void interpret(char *text)
   // tokenizer( pasa texto a token);
   get_next_token(text); // debe ser un int
   parse(INT);
-  printf("type=%d, value=%d\n", current_token.type, current_token.number_value);
- }
+  int left = current_token.number_value;
+  
+  get_next_token(text); // debe ser un operador
+  parse(OP);
+  char operator = current_token.operator_value;
+
+  get_next_token(text); // debe ser un int
+  parse(INT);
+  int right = current_token.number_value;
+
+  current_pos = 0;
+}
 
 int main()
 {
